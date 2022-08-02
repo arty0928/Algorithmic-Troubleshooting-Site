aa1185f8206371ef8237b7a23dbe92b0c924d633
HxNi <31992686+HxNi@users.noreply.github.com>
2022-08-03 03:54:18 +0900

    structure change

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const indexRouter = require('./routes/index');
-const problemRouter = require('./routes/problem');
+const apiRouter = require('./routes/api');
 
 const app = express();
 
-app.use('/', indexRouter);
-app.use('/problem', problemRouter);
+app.use('/api', apiRouter);
 app.use(express.static('public'));
 
 app.listen(5000, _=>console.log("SERVER STARTED"));
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.redirect('/problem.html');
+    res.send('hi');
 });
 
 module.exports = router;
