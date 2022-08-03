337644ed8325131d87530b1c9b13e89b1d717437
HxNi <31992686+HxNi@users.noreply.github.com>
2022-08-03 17:05:53 +0900

    minor

--- public/problem.js
+++ public/problem.js
@@ -1,4 +1,4 @@
 import './bundle.js';
 const {ReactDOM, Problem} = bundle;
-const problem = ReactDOM.createRoot(document.getElementById('problem'));
-problem.render(Problem());
+const root = ReactDOM.createRoot(document.getElementById('problem'));
+root.render(Problem());
