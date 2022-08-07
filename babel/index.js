91c558453d0a5afffb12ee886dc750c113a054ef
HxNi <31992686+HxNi@users.noreply.github.com>
2022-08-07 17:01:30 +0900

    Revert "delete test(UserButton)"
    This reverts commit e259a5adacbc6b40b23327abcf7503d9c2116173.

--- babel/index.js
+++ babel/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import UserButton from './UserButton';
 import Problem from './Problem/Problem';
 export default {
   React,
   ReactDOM,
+  UserButton,
   Problem
 };
