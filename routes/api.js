42715d66a3e4e9260fa3d6aa5632b55b4ac13e7f
HxNi <31992686+HxNi@users.noreply.github.com>
2022-08-07 13:15:32 +0900

    problem api: header add

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/problem', (req, res)=>{
-    res.json([{title: 'aaa', content: 'bbb'}, {title:'ccc', content: 'ddd'}]);
+    res.json([{pageNumber: 3, header: ['문제번호', '문제제목']}, {title: 'aaa', content: 'bbb'}, {title:'ccc', content: 'ddd'}]);
 });
 
 module.exports = router;
