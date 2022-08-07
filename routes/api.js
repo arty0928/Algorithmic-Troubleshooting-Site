630819c7d06b81d94b8e4951bdd7c8b283bc7915
HxNi <31992686+HxNi@users.noreply.github.com>
2022-08-07 18:28:14 +0900

    problem modeled

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 const router = Router();
 
+const problem = require('../models/problem');
+
 router.get('/', (req, res) => {
     res.send('hi');
 });
 
 router.get('/problem', (req, res)=>{
-    res.json([{pageNumber: 3, header: ['문제번호', '문제제목']}, {title: 'aaa', content: 'bbb'}, {title:'ccc', content: 'ddd'}]);
+    res.json([{pageNumber: 3, header: ['문제번호', '문제제목']},
+        ...problem
+    ]);
 });
 
 module.exports = router;
