e259a5adacbc6b40b23327abcf7503d9c2116173
HxNi <31992686+HxNi@users.noreply.github.com>
2022-08-07 17:01:12 +0900

    delete test(UserButton)

--- babel/index.js
+++ babel/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import UserButton from './UserButton';
 import Problem from './Problem/Problem';
 export default {
   React,
   ReactDOM,
-  UserButton,
   Problem
 };
