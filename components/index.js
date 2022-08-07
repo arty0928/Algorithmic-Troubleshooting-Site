eaa4b579ad530580d8f87911e60d03d90eb55120
HxNi <31992686+HxNi@users.noreply.github.com>
2022-08-07 17:03:53 +0900

    delete test(UserButton)

--- components/index.js
+++ components/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import UserButton from './UserButton';
 import Problem from './Problem/Problem';
 
-export default {React, ReactDOM, UserButton, Problem};
+export default {React, ReactDOM, Problem};
