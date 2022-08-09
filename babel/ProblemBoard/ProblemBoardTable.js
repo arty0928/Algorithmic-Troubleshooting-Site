a63debe2635b3a8f60163e6c8296136c4f42ed78
HxNi <31992686+HxNi@users.noreply.github.com>
2022-08-09 15:51:09 +0900

    rank image adjust

--- babel/ProblemBoard/ProblemBoardTable.js
+++ babel/ProblemBoard/ProblemBoardTable.js
@@ -1,5 +1,17 @@
 import React from 'react';
 const headerWidth = ["10%", "60%", "10%", "10%", "10%"];
+const rankImage = {
+  bronze5: "/img/rank/1.svg",
+  bronze4: "/img/rank/2.svg",
+  bronze3: "/img/rank/3.svg",
+  bronze2: "/img/rank/4.svg",
+  bronze1: "/img/rank/5.svg",
+  silver5: "/img/rank/6.svg",
+  silver4: "/img/rank/7.svg",
+  silver3: "/img/rank/8.svg",
+  silver2: "/img/rank/9.svg",
+  silver1: "/img/rank/10.svg"
+};
 
 const ProblemBoardTable = ({
   problemBoardHeader,
@@ -21,7 +33,13 @@ const ProblemBoardTable = ({
     href: `/problem/${val.id}`
   }, val.header.title)), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("a", {
     href: `/problem/${val.id}`
-  }, val.header.rank)), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("a", {
+  }, /*#__PURE__*/React.createElement("img", {
+    src: rankImage[val.header.rank],
+    style: {
+      width: "1.2em",
+      height: "1.2em"
+    }
+  }))), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("a", {
     href: `/problem/${val.id}`
   }, val.header.source)), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("a", {
     href: `/problem/${val.id}`
--- components/ProblemBoard/ProblemBoardTable.js
+++ components/ProblemBoard/ProblemBoardTable.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 const headerWidth = ["10%", "60%", "10%", "10%", "10%"];
+const rankImage = {
+    bronze5: "/img/rank/1.svg",
+    bronze4: "/img/rank/2.svg",
+    bronze3: "/img/rank/3.svg",
+    bronze2: "/img/rank/4.svg",
+    bronze1: "/img/rank/5.svg",
+    silver5: "/img/rank/6.svg",
+    silver4: "/img/rank/7.svg",
+    silver3: "/img/rank/8.svg",
+    silver2: "/img/rank/9.svg",
+    silver1: "/img/rank/10.svg",
+};
 
 const ProblemBoardTable = ({problemBoardHeader, problemBoardList}) => {
     return <table className="table table-bordered">
@@ -14,7 +26,8 @@ const ProblemBoardTable = ({problemBoardHeader, problemBoardList}) => {
             <tr key={index}>
                 <td><a href={`/problem/${val.id}`}>{val.id}</a></td>
                 <td><a href={`/problem/${val.id}`}>{val.header.title}</a></td>
-                <td><a href={`/problem/${val.id}`}>{val.header.rank}</a></td>
+                <td><a href={`/problem/${val.id}`}>
+                    <img src={rankImage[val.header.rank]} style={{width: "1.2em", height: "1.2em"}}/></a></td>
                 <td><a href={`/problem/${val.id}`}>{val.header.source}</a></td>
                 <td><a href={`/problem/${val.id}`}>{val.header.problemId}</a></td>
             </tr>
