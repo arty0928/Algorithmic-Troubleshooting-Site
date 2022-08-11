b5ed8dcbc9e9d0621d5a4cb2d85d65c33bb395a5
HxNi <31992686+HxNi@users.noreply.github.com>
2022-08-11 18:53:28 +0900

    Revert "merging"
    This reverts commit 8fde70a6d4a564f7d3df24b67a5c81da7d613f60.

--- babel/ProblemBoard/ProblemRegisterButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ProblemRegisterButton = () => {
-  return /*#__PURE__*/React.createElement("a", {
-    href: "/problem/register"
-  }, /*#__PURE__*/React.createElement("button", null, "\uB4F1\uB85D"));
-};
-
-export default ProblemRegisterButton;
