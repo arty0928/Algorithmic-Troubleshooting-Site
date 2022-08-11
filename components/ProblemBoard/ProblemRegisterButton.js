7dac9ab8276e49e85c97768517ba87dee3b8f0dc
arty0928 <arty0928@konkuk.ac.kr>
2022-08-12 00:18:49 +0900

    .

--- components/ProblemBoard/ProblemRegisterButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ProblemRegisterButton = () => {
-    return <a href="/problem/register"><button>등록</button></a>;
-};
-
-export default ProblemRegisterButton;
--- public/problemBoard.js
+++
@@ -1,4 +0,0 @@
-import './bundle.js';
-const {React, ReactDOM, BrowserRouter, StaticRouter, ProblemBoard} = bundle;
-const root = ReactDOM.createRoot(document.getElementById('problemBoard'));
-root.render(React.createElement(BrowserRouter, null, React.createElement(ProblemBoard)));
